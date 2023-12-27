e226164092569f6e57a52252f629f04864d7d1cf
XaviMV <70759474+XaviMV@users.noreply.github.com>
2023-12-27 13:18:31 +0100

    Add files via upload

---
+++ funcions_arduino.py
@@ -0,0 +1,112 @@
+from Arduino import Arduino
+import time as t
+
+# Fitxer per guardar funcions relacionades amb controlar la arduino
+
+class arduino_info:
+	def __init__(self):
+		# Pins a utilitzar, mirar que els pins dels servos tinguin implementada la funcionalitat pwm
+		self.llegir_pins()
+
+		try:
+			self.board = self.crear_board()
+		except:
+			self.board = 1
+
+	def crear_board(self):
+		self.board = iniciar_board(self)
+
+	def llegir_pins(self):
+		with open("data/arduino_pins.txt", 'r') as file:
+			linies = file.readlines()
+			self.PIN_SERVO_1 = int(linies[0])
+			self.PIN_SERVO_2 = int(linies[1])
+
+			self.PIN_STEPPER_1_STEP = int(linies[2])
+			self.PIN_STEPPER_2_STEP = int(linies[3])
+
+			self.PIN_STEPPER_1_DIR = int(linies[4])
+			self.PIN_STEPPER_2_DIR = int(linies[5])
+
+
+
+# funcio per moure els sevos amb les pwm donades
+def moure_servos(pwm1, pwm2, a):
+
+	a.board.analogWrite(a.PIN_SERVO_1, pwm1)
+	a.board.analogWrite(a.PIN_SERVO_2, pwm2)
+
+
+def moure_steppers(steppers_clicats, stepper1_invertit, stepper2_invertit, a):
+
+	# mirar que nomes es cliqui 1 sol boto
+	clicat = 0
+	for i in range(len(steppers_clicats)):
+		if steppers_clicats[i] == 1:
+			if clicat != 0: # si es troba un segon boto clicat es para la funcio
+				return
+			clicat = i
+
+
+	# Segur que hi ha una forma molt millor de tractar les direccions dels steppers si estan invertits pero estic massa cansat com per pensar en com fer-la
+
+	if clicat % 2 == 0: # S'ha clicat alguna fletxa superior (va cap a X = 0)
+		if not stepper1_invertit:
+			a.board.digitalWrite(a.PIN_STEPPER_1_DIR, "LOW")
+		else:
+			a.board.digitalWrite(a.PIN_STEPPER_1_DIR, "HIGH")
+
+		if not stepper2_invertit:
+			a.board.digitalWrite(a.PIN_STEPPER_2_DIR, "LOW")
+		else:
+			a.board.digitalWrite(a.PIN_STEPPER_2_DIR, "HIGH")
+	else:	# Si ha sigut una fletxa inferior
+		if stepper1_invertit:
+			board.digitalWrite(a.PIN_STEPPER_1_DIR, "LOW")
+		else:
+			a.board.digitalWrite(a.PIN_STEPPER_1_DIR, "HIGH")
+
+		if stepper2_invertit:
+			a.board.digitalWrite(a.PIN_STEPPER_2_DIR, "LOW")
+		else:
+			a.board.digitalWrite(a.PIN_STEPPER_2_DIR, "HIGH")
+
+
+	temps_per_step = 0.01
+	iteracions = 1
+	if clicat == 2 or clicat == 6: # s'han de fer 20 steps (fletxa gran)
+		iteracions = 30
+		temps_per_step = 0.004
+	
+	pin_stepper = 0
+	if clicat < 4: # stepper 1
+		pin_stepper = a.PIN_STEPPER_1_STEP
+	else: # stepper 2
+		pin_stepper = a.PIN_STEPPER_2_STEP
+
+	for i in range(iteracions):
+		a.board.digitalWrite(pin_stepper, "HIGH")
+		t.sleep(0.01)
+		a.board.digitalWrite(pin_stepper, "LOW")
+		t.sleep(0.01)
+
+def iniciar_board(a):
+	board = Arduino("115200")
+
+	board.pinMode(a.PIN_SERVO_1, "OUTPUT")
+	board.pinMode(a.PIN_SERVO_2, "OUTPUT")
+	board.pinMode(a.PIN_STEPPER_1_STEP, "OUTPUT")
+	board.pinMode(a.PIN_STEPPER_2_STEP, "OUTPUT")
+	board.pinMode(a.PIN_STEPPER_1_DIR, "OUTPUT")
+	board.pinMode(a.PIN_STEPPER_2_DIR, "OUTPUT")
+
+	# important no deixar els valors dels steppers flotant perque llavors es poden moure sols
+	board.digitalWrite(a.PIN_STEPPER_1_STEP, "LOW")
+	board.digitalWrite(a.PIN_STEPPER_2_STEP, "LOW")
+
+	board.digitalWrite(a.PIN_STEPPER_1_DIR, "LOW")
+	board.digitalWrite(a.PIN_STEPPER_2_DIR, "LOW")
+
+	moure_servos(128, 128, board)
+
+	return board
---
+++ main.py
@@ -0,0 +1,61 @@
+import math
+import os
+
+from funcions_arduino import iniciar_board, arduino_info
+from calibrar import calibrar
+from dibuixar_trajecte import dibuixar_trajecte, guardar_path, llegir_path
+from fer_trajecte import fer_trajecte
+
+
+# POSICIO DEL ORIGEN: el punt (0, 0) esta amb la x al cap de la maquina i la y en la posicio mes baixa
+
+
+
+# hi ha un try dintre de la creacio de la classe per si no esta conectada ningua arduino
+a = arduino_info()
+
+while True:
+
+	print("Que vols fer?")
+	print("    1.- Calibrar")
+	print("    2.- Crear trajecte")
+	print("    3.- Fer trajecte")
+	print("    4.- Sortir")
+
+	entrada = int(input())
+
+
+
+	if entrada == 1:
+		min_pwm1, max_pwm1, min_pwm2, max_pwm2, stepper1_invertit, stepper2_invertit, max_y_height = calibrar(a)
+
+	elif entrada == 2:
+		punts = dibuixar_trajecte(4)
+
+		print("Quin nom li vols posar al path creat? Deixa buit per no posar nom.")
+
+		nom = input()
+		if nom != " " and len(nom) != 0:
+			guardar_path(nom, punts)
+
+	elif entrada == 3:
+
+		min_pwm1, max_pwm1, min_pwm2, max_pwm2, stepper1_invertit, stepper2_invertit, max_y_height = calibrar(a)
+		
+		servo_info = min_pwm1, max_pwm1, min_pwm2, max_pwm2, 0, max_y_height
+
+		print("De quin arxiu guardat vols llegir el path a fer?")
+
+		for arx in os.listdir("data/created_paths"):
+			print(arx)
+
+		nom_arxiu = input()
+
+		punts = llegir_path(nom_arxiu)
+
+		print(punts)
+
+		x, y = fer_trajecte(punts, 1, servo_info, stepper1_invertit, stepper2_invertit, a)
+	
+	elif entrada == 4:
+		break
